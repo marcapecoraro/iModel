c49f9cc81741cbf05c7f0c6943b2db7514ffd51d
marcapecoraro <marca.pecoraro@gmail.com>
2018-11-19 15:47:48 +0100

    Add outles

--- iModel/Controller/SignInSignUpVC.swift
+++ iModel/Controller/SignInSignUpVC.swift
@@ -3,11 +3,25 @@ import UIKit
 
 class SignInSignUpVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet weak var signInButton: UIButton!
+	@IBOutlet weak var signUpButton: UIButton!
+	@IBOutlet weak var skipButton: UIButton!
 
-		
-        
+	override func viewDidLoad() {
+        super.viewDidLoad()
     }
+
+
+	@IBAction func signIn(_ sender: Any) {
+		print("signIn")
+	}
+
+	@IBAction func signUp(_ sender: Any) {
+		print("signUp")
+	}
+
+	@IBAction func skip(_ sender: Any) {
+		print("skip")
+	}
 
 }
